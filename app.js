e29519a504e65fbb684d6409070c4cc9b7918473
Vivek Vaibhav <vivek@oriserve.com>
2020-05-31 13:24:15 +0530

    404 page setup done

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require('path')
+
+const publicDirPath = path.join(__dirname, 'public')
 
 const port = 1623
 
 const app = express()
 
+app.use(express.static(publicDirPath))
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+
+
+app.use((req, res, next) => {
+  res.render('404', { pageTitle: '404', path: '/404' })
+})
 
 app.listen(port, () => {
   console.log(`server is up on port ${port}`)
